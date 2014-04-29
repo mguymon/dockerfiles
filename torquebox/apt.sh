64f184259395db40f74416a9d6e541cf224bada9
Michael Guymon <michael@tobedevoured.com>
2014-04-29 13:39:07 -0700

    use oracle java8

--- torquebox/apt.sh
+++ torquebox/apt.sh
@@ -2,14 +2,10 @@
 
 # move to own script to fight the AUF 42 parent max
 
-rm /var/lib/dpkg/info/oracle-java7-installer*
-apt-get purge oracle-java7-installer*
-rm /etc/apt/sources.list.d/*java*
-
 echo deb http://archive.ubuntu.com/ubuntu raring main universe multiverse > /etc/apt/sources.list
 apt-get update && apt-get install -y software-properties-common 
 add-apt-repository ppa:webupd8team/java -y 
 apt-get update
-echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-apt-get install -y oracle-java7-installer unzip wget
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+apt-get install -y oracle-java8-installer unzip wget
 
