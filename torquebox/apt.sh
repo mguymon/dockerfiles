bb538da4ecec8e60b4a98f3261eb907085fe35ca
Michael Guymon <michael@tobedevoured.com>
2014-04-29 12:05:46 -0700

    get latest oracle 7 to install

--- torquebox/apt.sh
+++ torquebox/apt.sh
@@ -2,6 +2,10 @@
 
 # move to own script to fight the AUF 42 parent max
 
+rm /var/lib/dpkg/info/oracle-java7-installer*
+apt-get purge oracle-java7-installer*
+rm /etc/apt/sources.list.d/*java*
+
 echo deb http://archive.ubuntu.com/ubuntu raring main universe multiverse > /etc/apt/sources.list
 apt-get update && apt-get install -y software-properties-common 
 add-apt-repository ppa:webupd8team/java -y 
