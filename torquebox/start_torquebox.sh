cdfb6dcf672e1c56afc11b17500d64041f722c69
Michael Guymon <michael@tobedevoured.com>
2014-05-13 10:03:39 -0400

    fix start path

--- torquebox/start_torquebox.sh
+++ torquebox/start_torquebox.sh
@@ -3,4 +3,4 @@
 
 IP=`ip addr list eth0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1`
 
-/opt/torquebox/current/jboss/bin/standalone.sh -Djboss.bind.address=$IP
+/opt/torquebox/jboss/bin/standalone.sh -Djboss.bind.address=$IP
